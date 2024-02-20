aacf58a7acba5e89d8bb423cbf9f8654101f7d67
Amin <amnp95@uw.edu>
2024-02-19 18:30:28 -0800

    adding mesh generator project

---
+++ MeshGenerator/build.bat
@@ -0,0 +1,73 @@
+@echo off
+cd GKlib
+REM Create directories
+mkdir build
+mkdir GKLIBRARY
+
+REM Set environment variable
+set GKLIBRARY=%CD%\GKLIBRARY
+
+REM Change directory to build
+cd build
+
+REM Set GKLIBRARY environment variable to the full path of the GKLIBRARY directory
+
+REM Assuming the second "cd build" is a mistake since we're already in "build" directory. 
+REM If it's meant to go into another "build" directory inside the first, uncomment the next line:
+REM cd build
+
+REM Run cmake commands
+cmake .. -DCMAKE_INSTALL_PREFIX=%GKLIBRARY%
+cmake --build . --config Release --target GKlib
+cmake --install . 
+cd ..
+mkdir  GKLIBRARY\include\win32
+copy win32\adapt.h GKLIBRARY\include\win32
+cd ..
+
+
+
+set "GKLIBRARY=%CD%\GKlib\GKLIBRARY"
+cd METIS
+REM Set METISLIBRARY to the current directory followed by \METISLIBRARY
+mkdir build
+mkdir build\windows
+mkdir build\xinclude
+mkdir METISLIBRARY
+set "METISLIBRARY=%CD%\METISLIBRARY"
+
+
+REM Echo IDXWIDTH and REALWIDTH environment variables to metis.h
+set "IDXWIDTH=#define IDXTYPEWIDTH 32"
+set "REALWIDTH=#define REALTYPEWIDTH 32"
+echo %IDXWIDTH% >> build\xinclude\metis.h
+echo %REALWIDTH% >> build\xinclude\metis.h
+
+REM Concatenate the contents of include\metis.h to build\xinclude\metis.h
+type include\metis.h >> build\xinclude\metis.h
+
+REM Copy include\CMakeLists.txt to build\xinclude
+copy include\CMakeLists.txt build\xinclude
+
+REM Change directory to build
+cd build\windows
+
+REM Run cmake commands with the previously set environment variables
+cmake  -DCMAKE_INSTALL_PREFIX=%METISLIBRARY% -DGKLIB_PATH=%GKLIBRARY% ..\..
+cmake --build . --config Release
+cmake --install . --prefix %METISLIBRARY% 
+cd ..\..\..
+
+cd Partitioner
+mkdir build
+cd build
+cmake .. -DGKLIB_PATH=%GKLIBRARY% -DMETIS_PATH=%METISLIBRARY%
+cmake --build . --config Release
+cmake --install . 
+cd ../..
+
+python test.py
+
+
+
+
---
+++ MeshGenerator/test.py
@@ -0,0 +1,44 @@
+# we want to import D:\Projects\Mesh\Partitioner\build\Release\PartitionLib.dll which is  C library in python and use it 
+ # %%
+import ctypes
+import numpy as np
+import os
+import pyvista as pv
+
+#// load the library
+metis_partition_lib = ctypes.CDLL('./lib/Partitioner.dll')
+# Define function argument and return types
+metis_partition_lib.Partition.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_int32), ctypes.c_int, ctypes.POINTER(ctypes.c_int32)]
+metis_partition_lib.Partition.restype = ctypes.c_int
+
+# %%
+# Create a simple mesh
+x = np.arange(-10, 10+0.01, 1.0)
+y = np.arange(-10, 10+0.01, 1.0)
+z = np.arange(-10, 10+0.01, 1.0)
+
+x, y, z = np.meshgrid(x, y, z)
+
+grid = pv.StructuredGrid(x, y, z)
+
+# change grid to unstructured grid
+grid = grid.cast_to_unstructured_grid()
+
+
+numcores = 4
+numcells = grid.n_cells
+numpoints = grid.n_points
+numweights = 1
+
+
+cells = np.array(grid.cells.reshape(-1, 9), dtype=np.int32)
+cells = cells[:,1:]
+# %%
+cellspointer = cells.flatten().ctypes.data_as(ctypes.POINTER(ctypes.c_int32))
+partitioned = np.empty(numcells, dtype=np.int32)
+partitionedpointer = partitioned.ctypes.data_as(ctypes.POINTER(ctypes.c_int32))
+metis_partition_lib.Partition(numcells, numpoints, cellspointer, numcores, partitionedpointer)
+grid.cell_data['partitioned'] = partitioned
+grid.plot(show_edges=False, scalars='partitioned', cmap='rainbow')
+
+# %%
